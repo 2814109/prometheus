7d2002d3e5cf9ab792d713a3fac34536a195f3b4
kirke <kirke@kirkenoMacBook-Air.local>
2022-08-14 01:21:39 +0900

    connect to db

--- prometheus-backend/src/app.module.ts
+++ prometheus-backend/src/app.module.ts
@@ -1,27 +1,60 @@
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { DirectiveLocation, GraphQLDirective } from 'graphql';
-import { upperDirectiveTransformer } from './common/directives/upper-case.directive';
-import { RecipesModule } from './recipes/recipes.module';
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { DirectiveLocation, GraphQLDirective } from "graphql";
+import { upperDirectiveTransformer } from "./common/directives/upper-case.directive";
+import { RecipesModule } from "./recipes/recipes.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import configuration from "./config/configuration";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+
     RecipesModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: 'schema.gql',
-      transformSchema: schema => upperDirectiveTransformer(schema, 'upper'),
+      autoSchemaFile: "schema.gql",
+      transformSchema: (schema) => upperDirectiveTransformer(schema, "upper"),
       installSubscriptionHandlers: true,
       buildSchemaOptions: {
         directives: [
           new GraphQLDirective({
-            name: 'upper',
+            name: "upper",
             locations: [DirectiveLocation.FIELD_DEFINITION],
           }),
         ],
       },
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get("database.host"),
+        port: Number(configService.get("database.port")),
+        username: configService.get("database.user"),
+        password: configService.get("database.pass"),
+        database: configService.get("database.name"),
+        extra: {},
+      }),
+      inject: [ConfigService],
+    }),
+
+    // TypeOrmModule.forRoot({
+    //   keepConnectionAlive: true,
+    //   type: "postgres",
+    //   host: process.env.POSTGRES_URI,
+    //   port: Number(process.env.POSTGRES_PORT),
+    //   username: process.env.POSTGRES_USER,
+    //   password: process.env.POSTGRES_PASSWORD,
+    //   database: process.env.POSTGRES_DB,
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
   ],
 })
 export class AppModule {}
